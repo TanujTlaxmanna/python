55273fc9c6c85ae1492d07e8ab2e9ef51f513a37
TanujTlaxmanna <tanujlaxmanna3107@gmail.com>
2026-06-13 16:25:20 +0530

    Data Frames

--- pandas_practise.py
+++ pandas_practise.py
@@ -2,64 +2,64 @@
 
 import pandas as pd
 
-x = [3,4,5,6,7,8,9]
-a = pd.Series(x, index = ['a', 'b', 'c', 'd', 'e', 'f', 'g'], dtype = "float", name = "Pythong")
-print(a)
-print(type(a))
-print(x[5])
+# x = [3,4,5,6,7,8,9]
+# a = pd.Series(x, index = ['a', 'b', 'c', 'd', 'e', 'f', 'g'], dtype = "float", name = "Pythong")
+# print(a)
+# print(type(a))
+# print(x[5])
 
-dic = {"Name" : ["Python", "Java", "C#", "C++"],
-        "Por" : [12,13,14,15],
-        "Rank" : [1,4,3,2]}
+# dic = {"Name" : ["Python", "Java", "C#", "C++"],
+#         "Por" : [12,13,14,15],
+#         "Rank" : [1,4,3,2]}
 
-var1 = pd.Series(dic)
-print(var1)
+# var1 = pd.Series(dic)
+# print(var1)
 
-s = pd.Series(12, index = [1,2,3,4,5,6,7])
-print(s)
-print(type(s))
+# s = pd.Series(12, index = [1,2,3,4,5,6,7])
+# print(s)
+# print(type(s))
 
-s1 = pd.Series(12, index = [1,2,3,4,5,6,7])
-s2 = pd.Series(12, index = [1,2,3,4])
+# s1 = pd.Series(12, index = [1,2,3,4,5,6,7])
+# s2 = pd.Series(12, index = [1,2,3,4])
 
-print(s1+s2)
+# print(s1+s2)
 
 
 # DATAFRAMES - 2D STRUCTURE
 
 # using lists
-# l = [1,2,3,4,5,6]
-# var = pd.DataFrame(l)
-# print(var)
-# print(type(var))
+l = [1,2,3,4,5,6]
+var = pd.DataFrame(l)
+print(var)
+print(type(var))
 
-# list1 = [[1,2,3,4,5], [6,7,8,9,0]]
-# var2 = pd.DataFrame(list1)
-# print(var2)
+list1 = [[1,2,3,4,5], [6,7,8,9,0]]
+var2 = pd.DataFrame(list1)
+print(var2)
 
 # using dictionaaries
 
-# d = {"a" : [1,2,3,4,5],
-#      "b" : [6,7,8,9,0]}
+d = {"a" : [1,2,3,4,5],
+     "b" : [6,7,8,9,0]}
 
-# var1 = pd.DataFrame(d)
-# print(var1)
-# print(type(var1))
+var1 = pd.DataFrame(d)
+print(var1)
+print(type(var1))
 
 # specific columns
 
-# var1 = pd.DataFrame(d, columns= ["a"], index = ["a", "b", "c", "d", "e"])
-# print(var1, "\n")
-# print(d["a"][3])
+var1 = pd.DataFrame(d, columns= ["a"], index = ["a", "b", "c", "d", "e"])
+print(var1, "\n")
+print(d["a"][3])
 
 
 # Using series
 
-# sr = {"s" : pd.Series([1,2,3,4,5]),
-#       "r" : pd.Series([6,7,8,9,0])}
+sr = {"s" : pd.Series([1,2,3,4,5]),
+      "r" : pd.Series([6,7,8,9,0])}
 
-# var3 = pd.DataFrame(sr, index = [0,1,2,3,4])
-# print(var3)
+var3 = pd.DataFrame(sr, index = [0,1,2,3,4])
+print(var3)
 
 
 # Arithmetic Operations
