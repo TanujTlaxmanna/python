556e59afa0174e1a146cae925e4afbb57eaee1db
TanujTlaxmanna <tanujlaxmanna3107@gmail.com>
2026-06-13 15:06:55 +0530

    Basic Data Structures

---
+++ pandas_practise.py
@@ -0,0 +1,25 @@
+# SERIES
+
+import pandas as pd
+
+# x = [3,4,5,6,7,8,9]
+# a = pd.Series(x, index = ['a', 'b', 'c', 'd', 'e', 'f', 'g'], dtype = "float", name = "Pythong")
+# print(a)
+# print(type(a))
+# print(x[5])
+
+# dic = {"Name" : ["Python", "Java", "C#", "C++"],
+#         "Por" : [12,13,14,15],
+#         "Rank" : [1,4,3,2]}
+
+# var1 = pd.Series(dic)
+# print(var1)
+
+# s = pd.Series(12, index = [1,2,3,4,5,6,7])
+# print(s)
+# print(type(s))
+
+# s1 = pd.Series(12, index = [1,2,3,4,5,6,7])
+# s2 = pd.Series(12, index = [1,2,3,4])
+
+# print(s1+s2)
