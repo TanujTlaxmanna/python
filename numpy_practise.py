35ef39eafeb46efa70e835212c8a2662d7c902dc
TanujTlaxmanna <tanujlaxmanna3107@gmail.com>
2026-06-15 16:50:06 +0530

    Numpy Datatypes

--- numpy_practise.py
+++ numpy_practise.py
@@ -114,34 +114,91 @@ import numpy as np
 
 # RAND()
 
-var = np.random.rand(4)
-print(var)
+# var = np.random.rand(4)
+# print(var)
 
-var1 = np.random.rand(2,5)   # 2-D array
-print(var1)
+# var1 = np.random.rand(2,5)   # 2-D array
+# print(var1)
 
 
 # RANDN()
 
-var2 = np.random.randn(5)
-print(var2)
+# var2 = np.random.randn(5)
+# print(var2)
 
-var3 = np.random.randn(3,5) # 2-D array
-print(var3)
+# var3 = np.random.randn(3,5) # 2-D array
+# print(var3)
 
 
 # RANF()
 
-var4 = np.random.ranf(4)
-print(var4)
+# var4 = np.random.ranf(4)
+# print(var4)
 
-var5 = np.random.ranf((4,5)) # 2-D ARRAY
-print(var5)
+# var5 = np.random.ranf((4,5)) # 2-D ARRAY
+# print(var5)
 
 # RANDINT()
 
-var6 = np.random.randint(5,20,5)
-print(var6)
+# var6 = np.random.randint(5,20,5)
+# print(var6)
 
-var7 = np.random.randint(5,20,(4,5)) #2-D ARRAY
-print(var7)
+# var7 = np.random.randint(5,20,(4,5)) #2-D ARRAY
+# print(var7)
+
+
+# DATA TYPES
+
+var1 = np.array([1,2,3,4,5,6,7,8,9,8,7,6,5,4,43,32,1,5235,321,4,55,3,532,532,5])
+print("Data Type : ", var1.dtype)
+
+var2 = np.array([1.0, 2.2, 3.3, 4.0])
+print("Data Type : ", var2.dtype)
+
+var3 = np.array(["A", "B", "C", "D"])
+print("Data Type : ", var3.dtype)
+
+var4 = np.array(["A", "B", "C", "D", 1, 2, 3, 4])
+print("Data Type : ", var4.dtype)
+
+
+# ARRAY CONVERSION
+
+x = np.array([1,2,3,4])
+print("Data Type : ", x.dtype)
+
+x = np.array([1,2,3,4], dtype = np.int8)
+print("Data Type : ", x.dtype)
+
+
+x1 = np.array([1,2,3,4], dtype = "f")
+print("Data Type : ", x1.dtype)
+
+x2 = np.array([1,2,3,4])
+new = np.float32(x2)
+
+print("Data Type : ", x2.dtype)
+print("Data Type : ", new.dtype)
+print(x2)
+print(new)
+
+
+x2 = np.array([1,2,3,4])
+new = np.float32(x2)
+new_1 = np.int_(new)
+
+print("Data Type : ", x2.dtype)
+print("Data Type : ", new.dtype)
+print("Data Type : ", new_1)
+print(x2)
+print(new)
+print(new_1)
+
+
+
+x3 = np.array([1,2,3,4])
+new1 = x3.astype(float)
+print("Data Type : ", x3)
+print("Data Type : ", new1)
+print(x3)
+print(new)
