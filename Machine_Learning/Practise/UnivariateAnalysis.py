8067b72b9f6be4405093f7943d3ea0918cefd87e
TanujTlaxmanna <tanujlaxmanna3107@gmail.com>
2026-06-30 15:59:29 +0530

    Univariate Analysis

--- Machine_Learning/Practise/UnivariateAnalysis.py
+++ Machine_Learning/Practise/UnivariateAnalysis.py
@@ -11,25 +11,25 @@ print(df.head())
 print(df['Survived'].value_counts())
 sns.countplot(x = 'Survived', data = df, palette=['red', 'green'], hue='Survived', legend = True)
 plt.legend(labels=['Dead', 'Survived'])
-plt.savefig("EDA Graphs\\Survived_count.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Survived_count.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
 
 print(df['Pclass'].value_counts())
 sns.countplot(x = 'Pclass', data = df, palette=['red', 'green', 'blue'], hue='Pclass', legend = True)
 plt.legend(labels = ['First Class', 'Second Class', 'Third Class'])
-plt.savefig("EDA Graphs\\Pclass_count.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Pclass_count.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
 
 print(df['Sex'].value_counts())
 sns.countplot(x = 'Sex', data = df, palette=['red', 'green'], hue='Sex', legend = True)
 plt.legend(labels=['Male', 'Female'])
-plt.savefig("EDA Graphs\\Sex_count.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Sex_count.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
 
 print(df['Embarked'].value_counts())
 sns.countplot(x = 'Embarked', data = df, palette=['red', 'green', 'blue'], hue='Embarked', legend = True)
 plt.legend(labels = ['Cherbourg', 'Queenstown', 'Southampton'])
-plt.savefig("EDA Graphs\\Embarked_count.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Embarked_count.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
 
 
@@ -39,28 +39,28 @@ plt.show()
 df['Survived'].value_counts().plot(kind='pie', autopct='%.2f%%')
 plt.title("Survival Distribution")
 plt.ylabel("")
-plt.savefig("EDA Graphs\\Survived_pie.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Survived_pie.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
 
 # Passenger Class
 df['Pclass'].value_counts().plot(kind='pie', autopct='%.2f%%')
 plt.title("Passenger Class Distribution")
 plt.ylabel("")
-plt.savefig("EDA Graphs\\Pclass_pie.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Pclass_pie.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
 
 # Sex
 df['Sex'].value_counts().plot(kind='pie', autopct='%.2f%%')
 plt.title("Gender Distribution")
 plt.ylabel("")
-plt.savefig("EDA Graphs\\Sex_pie.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Sex_pie.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
 
 # Embarked
 df['Embarked'].value_counts().plot(kind='pie', autopct='%.2f%%')
 plt.title("Embarkation Port Distribution")
 plt.ylabel("")
-plt.savefig("EDA Graphs\\Embarked_pie.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Embarked_pie.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
 
 
@@ -69,7 +69,7 @@ plt.show()
 #HISTOGRAM
 print(df['Age'].value_counts())
 plt.hist(df['Age'], bins = 5)
-plt.savefig("EDA Graphs\\Age_Histo.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Age_Histo.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
 
 # DISTPLOT (removed from newer seabord versions, kde is kernal destribution estimation(the line curve))
@@ -77,7 +77,7 @@ sns.histplot(df['Age'], kde=True, color='royalblue')
 plt.title("Age Distribution")
 plt.xlabel("Age")
 plt.ylabel("Count")
-plt.savefig("EDA Graphs\\Age_histplot.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Age_histplot.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
 
 
@@ -85,5 +85,6 @@ plt.show()
 sns.boxplot(x=df['Fare'])
 plt.title("Fare Box Plot")
 plt.xlabel("Fare")
-plt.savefig("EDA Graphs\\Fare_boxplot.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
+plt.savefig("EDA Graphs\\Univariate\\Fare_boxplot.png", dpi=300, facecolor='lightpink', transparent=True, bbox_inches="tight")
 plt.show()
+
